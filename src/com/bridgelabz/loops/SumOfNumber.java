0d3173de107e487023198816b5f74316143c48be
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-07 22:51:53 +0530

    Added: All problems of Day2

---
+++ src/com/bridgelabz/loops/SumOfNumber.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.loops;
+
+import java.util.Scanner;
+
+public class SumOfNumber {
+    public static void main(String[] args) {
+        int n, i, sum =0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        n = sc.nextInt();
+
+        for(i=1; i<=n; i++)
+        {
+            sum = sum + i;
+        }
+        System.out.println(" The  Sum of Natural Numbers from 1 to " +n+ "=" +sum);
+    }
+}
---
+++ src/com/bridgelabz/whileloop/ReverseNum.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.whileloop;
+
+import java.util.Scanner;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        int reverse = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Numbers: ");
+        int num = sc.nextInt();
+
+        while(num != 0)
+        {
+            int reminder = num % 10;
+            reverse = reverse * 10 + reminder;
+            num = num/10;
+        }
+        System.out.println("The reverse number is : " +reverse);
+    }
+}
